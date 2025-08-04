93a958cf69053185d6af579c589941ebeb6b51c9
Bruno Pimentel <bruno.pimentel@usercentrics-ext.com>
2025-08-04 16:02:06 +0100

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,20 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Bruno Valente on 04/08/25.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ testTests/testTests.swift
@@ -0,0 +1,17 @@
+//
+//  testTests.swift
+//  testTests
+//
+//  Created by Bruno Valente on 04/08/25.
+//
+
+import Testing
+@testable import test
+
+struct testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
